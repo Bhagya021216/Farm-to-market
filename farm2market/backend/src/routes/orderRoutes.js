f753a7dbd72dd887c008b70d3ecff59e1eea2255
Bhagya Nethmini <bhagyakapuge@gmail.com>
2025-09-02 21:38:34 +0530

    Delete farm2market directory

--- farm2market/backend/src/routes/orderRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const auth = require("../middleware/auth");
-const { placeOrder, getMyOrders } = require("../controllers/orderController");
-const router = express.Router();
-
-router.post("/", auth, placeOrder);
-router.get("/", getMyOrders);
-
-module.exports = router;
